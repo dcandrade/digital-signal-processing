3e987271cc51dfdfc5fa66fc45f6de502a8c70f1
Daniel Andrade <dca650@gmail.com>
2017-10-01 18:48:32 -0300

    Refactoring

--- simulator.m
+++ simulator.m
@@ -8,27 +8,26 @@ freq = 1;           % Frequência (Hz)
 A = 4;              % Amplitude
 
 % Parâmetros da Animação
-periods = 2;                % Período (com base no período fundamental 2pi)
-num_points = 100;           % Quantidades de pontos total da senóide
-time_periods = (1/freq) * periods;
+periods = 10;                       % Quantidade de perídos da onda a serem exibidos
+num_points = 100;                   % Quantidades de pontos total da senóide
+time_periods = (1/freq) * periods;  % Cálculo do tempo total de execução
 
 % Gerando pontos da circunferência no plano (x, y)
-t = linspace(0, 2*pi, 50);
-x = cos(t)*A - A;  % Deslocando p/ antes da origem
-y = sin(t)*A;
+plot_circle = subplot(1,2,1);
+t = linspace(0, 2*pi);
+X = cos(t)*A ;
+Y = sin(t)*A;
 
 % Plotando circunferência
-line(x, y, 'LineWidth',2);                       % Plota circunferência
-line([0 0], [-A-1 A+1], 'Color','k')             % Plota eixo Y=0, separando circunferência da senóide
-grid on, box on
-axis equal
-axis([-2*A time_periods+1 -A A])
-xlabel('Ângulo \theta (radianos)')
-title('Efeito Estroboscópico')
+line(X, Y, 'LineWidth',2);
+grid on, box on, axis equal;
+xlabel('Vetor gerador da senóide');
 
 % Preparando objetos animados
-vector = line(ones(1,2), ones(1,2), 'LineWidth',1); % Inicializando linha
-senoidLine = line(ones(1,2), ones(1,2), 'LineWidth',1, 'LineStyle','-', 'Color', 'red');
+vector = line(ones(1,2), ones(1,2), 'LineWidth',1);
+plot_senoid = subplot(1,2,2);
+grid on, box on;
+axis([0 time_periods+1 -A A])
 
 % Preparando senoide
 senoidAnimation = animatedline('Color',vector.Color, 'LineWidth',2);
@@ -38,19 +37,18 @@ buildingSenoid = true;
 
 while true
     for t=0:1/num_points:time_periods
-
+        if ~isgraphics(hFig), return; end % Encerra se a figura foi fechada
+        
         % Atualiza coordenadas (x,y)
         x = A*cos(2*pi*freq*t);
         y = A*sin(2*pi*freq*t);
 
-        % Atualiza os gráficos com os pontos atuais
         if(buildingSenoid)
             addpoints(senoidAnimation, t, A*sin(2*pi*freq*t));
         end
 
-        set(senoidLine, 'XData',[x-A; t], 'YData',[y; y]);
-        set(vector, 'XData',[0; x] - A, 'YData',[0; y]);        % Desenha vetor girante
-            
+        set(vector, 'XData',[0; x], 'YData',[0; y]);        % Desenha vetor girante
+
         % Refresh nos gráficos
         drawnow;
     end
